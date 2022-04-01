45ba423ea2a3dbfec8ee7afc9b3c215455f611b0
Parietic <70056597+Parietic@users.noreply.github.com>
2022-04-01 16:17:39 +0100

    Altered app window settings

--- src/main/common/createWindow.js
+++ src/main/common/createWindow.js
@@ -4,11 +4,22 @@ const { BrowserWindow } = require("electron");
 module.exports = () => {
 	// Create the browser window.
 	_appWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 1280,
+		height: 720,
+		show: false,
+		autoHideMenuBar: true,
+		webPreferences: {
+			contextIsolation: true,
+			//preload: "./preload.js",
+		},
 	});
 
-	// and load the index.html of the app.
+	// Only show window when it has finished loading
+	_appWindow.once("ready-to-show", () => {
+		_appWindow.show();
+	});
+
+	// Load the index.html of the app.
 	_appWindow.loadFile(path.join(__dirname, "../../render/index.html"));
 
 	// Open the DevTools.
